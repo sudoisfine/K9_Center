508df80126ceeb5d01e709cfb1a9ad5307d90804
MELISSA GUION <M.GUION@GMAIL.COM>
2016-05-02 10:22:21 -0500

    data migration

---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160502152000) do
+
+  create_table "breeds", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string   "name"
+    t.date     "dob"
+    t.boolean  "checked_in"
+    t.integer  "owner_id_id"
+    t.integer  "vet_id_id"
+    t.text     "info"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "breed_id_id"
+  end
+
+  add_index "dogs", ["breed_id_id"], name: "index_dogs_on_breed_id_id"
+  add_index "dogs", ["owner_id_id"], name: "index_dogs_on_owner_id_id"
+  add_index "dogs", ["vet_id_id"], name: "index_dogs_on_vet_id_id"
+
+  create_table "owners", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "emergency_name"
+    t.string   "emergency_phone"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "vets", force: :cascade do |t|
+    t.string   "doctor"
+    t.string   "clinic"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end

2b4f60130cd525f0483244541f1a82caab4099cb
MELISSA GUION <M.GUION@GMAIL.COM>
2016-05-03 19:58:09 -0500

    wrote method to show paw print for checked in dogs in the dogs index view and show 'Yes' and 'No' in show view instead of boolean values

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,10 @@
 class Owner < ActiveRecord::Base
   has_many :dogs
 
+  def name
+    "#{first_name} #{last_name}"
+  end
+
   validates :name, presence: true, length: {minimum: 1}
   validates :phone, presence: true, length: {minimum: 7}, numericality: { only_integer: true }
 end
--- app/models/vet.rb
+++ app/models/vet.rb
@@ -1,3 +1,7 @@
 class Vet < ActiveRecord::Base
   has_many :dogs
+
+  def name
+    "#{doctor}"
+  end
 end
